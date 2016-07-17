46ae029df2eeda11afaea87b6fa8e4fbdb289d29
Darwing Hernandez <dhernandez@m5-software.com>
2016-07-17 00:14:47 -0400

    Fix paste error and document code

--- change_money.cpp
+++ change_money.cpp
@@ -1,39 +1,73 @@
-#include <iostream>
-using namespace std;
-
-int optimalSolution(int m)
-{
-	int total = 0;
-	if(m/10 > 0){ total += m/10;  m = m - (m/10)*10; }
-	if(m - 5 >= 0){ total += 1;  m = m - 5; }
-	if(m > 0){ total += m; }
-
-	return total;
-}
-
-int slowSolution(int m)
-{
-	int total = 0;
-	int idx = 0;
-
-	while(total < m)
-	{
-		if(total + 10 <= m) total += 10;
-		else if(total + 5 <= m) total += 5;
-		else if(total + 1 <= m) total++;
-		idx++;
-	}
-
-	return idx;
-}
-
-
-int main(int argc, char const *argv[])
-{
-	int m;
-	cin >> m;
-
-	cout << optimalSolution(m) << endl;
-
-	return 0;
+/**
+ * Purpose: Determines the minimum number of coins needed to change
+ * the input value with coins denomination 1, 5, and 10.
+ *
+ * This must be resolved with a greeding algorithm.
+ * 
+ * Input: 	a single integer; 1 <= m <= 10^3
+ * Output: 	minimum number of coins needed to change m with
+ * 			denomination 1, 5, and 10
+ */
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Optimal solution of changing money with the denomination given.
+ * The idea is to take a safe move, that is, always take the major
+ * denomination while you can. The diference here is that never
+ * iterates, just arithmetic operations with the m given.
+ * 
+ * @return   minimum number of coins
+ */
+int optimalSolution(int m)
+{
+	int total = 0;
+
+	if(m/10 > 0)
+	{ 
+		total += m/10;
+		m = m - (m/10)*10; 
+	}
+
+	if(m - 5 >= 0)
+	{
+		total += 1;
+		m = m - 5;
+	}
+
+	if(m > 0)
+		total += m;
+
+	return total;
+}
+
+/**
+ * Slow and naive solution. Iterates until gets to the amount, counting
+ * the coins used.
+ * 
+ * @return   minimum number of coins
+ */
+int slowSolution(int m)
+{
+	int total = 0;
+	int idx = 0;
+
+	while(total < m)
+	{
+		if(total + 10 <= m) total += 10;
+		else if(total + 5 <= m) total += 5;
+		else if(total + 1 <= m) total++;
+		idx++;
+	}
+	return idx;
+}
+
+int main(int argc, char const *argv[])
+{
+	int m;
+	cin >> m;
+	cout << optimalSolution(m) << endl;
+
+	return 0;
 }
--- fractional_knapsack.cpp
+++ fractional_knapsack.cpp
@@ -1,77 +1,78 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-using namespace std;
-
-struct item
-{
-	int value;
-	int weight;
-	double vw;
-};
-
-vector<item> items;
-
-void sortItems(int n)
-{
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n - 1; j++)
-		{
-			if(items[j].vw < items[j+1].vw)
-			{
-				item backup = items[j];
-				items[j] = items[j+1];
-				items[j+1] = backup;
-			}
-		}
-	}
-}
-
-double knapsack(double totalW, int n)
-{
-	double maxValue = 0.0;
-	int idx = 0;
-
-	while(totalW > 0)
-	{
-		if(items[idx].weight <= totalW)
-		{
-			totalW -= items[idx].weight;
-			maxValue += items[idx].value;
-		}
-		else
-		{
-			maxValue += items[idx].vw*totalW;
-			totalW -= totalW;
-		}
-
-		idx++;
-		if(idx >= n) break;
-	}
-
-	return maxValue;
-}
-
-int main(int argc, char const *argv[])
-{
-	int n;
-	double maxValue, totalW;
-	cin >> n >> totalW;
-
-	for(int i = 0; i < n; i++)
-	{
-		item ii;
-		cin >> ii.value >> ii.weight;
-		ii.vw = (double)ii.value/ii.weight;
-
-		items.push_back(ii);
-	}
-
-	sortItems(n);
-	maxValue = knapsack(totalW, n);
-
-	cout << fixed << setprecision(4) << maxValue << endl;
-
-	return 0;
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+struct item
+{
+	int value;
+	int weight;
+	double vw;
+};
+
+vector<item> items;
+
+void sortItems(int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n - 1; j++)
+		{
+			if(items[j].vw < items[j+1].vw)
+			{
+				item backup = items[j];
+				items[j] = items[j+1];
+				items[j+1] = backup;
+			}
+		}
+	}
+}
+
+double knapsack(double totalW, int n)
+{
+	double maxValue = 0.0;
+	int idx = 0;
+
+	while(totalW > 0)
+	{
+		if(items[idx].weight <= totalW)
+		{
+			totalW -= items[idx].weight;
+			maxValue += items[idx].value;
+		}
+		else
+		{
+			maxValue += items[idx].vw*totalW;
+			totalW -= totalW;
+		}
+
+		idx++;
+		if(idx >= n) break;
+	}
+
+	return maxValue;
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	double maxValue, totalW;
+	cin >> n >> totalW;
+
+	for(int i = 0; i < n; i++)
+	{
+		item ii;
+		cin >> ii.value >> ii.weight;
+		ii.vw = (double)ii.value/ii.weight;
+		items.push_back(ii);
+	}
+
+	sortItems(n);
+	maxValue = knapsack(totalW, n);
+	cout << fixed << setprecision(4) << maxValue << endl;
+
+	return 0;
 }
