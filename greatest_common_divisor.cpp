1c6a2b1681d28ffd2b2d8ce4813f4bb8c70471ad
Darwing Hernandez <dhernandez@m5-software.com>
2016-07-17 00:00:39 -0400

    Fix paste error and document code

--- greatest_common_divisor.cpp
+++ greatest_common_divisor.cpp
@@ -1,20 +1,27 @@
-#include <iostream>
-using namespace std;
-
-unsigned long gdc(unsigned long a, unsigned long b)
-{
-	if(b == 0) return a;
-	return gdc(b, a%b);
-}
-
-int main() 
-{
-	unsigned long a, b;
-	cin >> a >> b;
-	
-	if(a < b) swap(a,b);
-	
-	cout << gdc(a, b) << endl;
-	
-	return 0;
+/**
+ * Purpose: Calculates the greatest common divisor between two integers.
+ * 
+ * Input: 	two integers a,b; 1 <= a,b <= 2.10^9
+ * Output: 	GCD(a,b)
+ */
+
+#include <iostream>
+using namespace std;
+
+unsigned long gcd(unsigned long a, unsigned long b)
+{
+	if(b == 0) return a;
+	return gcd(b, a%b);
+}
+
+int main() 
+{
+	unsigned long a, b;
+	cin >> a >> b;
+
+	if(a < b) swap(a,b);
+
+	cout << gcd(a, b) << endl;
+
+	return 0;
 }
--- least_common_multiple.cpp
+++ least_common_multiple.cpp
@@ -1,20 +1,29 @@
-#include <iostream>
-using namespace std;
-
-unsigned long lcm(unsigned long a, unsigned long b)
-{
-	if(b == 0) return a;
-	return lcm(b, a%b);
-}
-
-int main() 
-{
-	unsigned long a, b;
-	cin >> a >> b;
-	
-	if(a < b) swap(a,b);
-	
-	cout << (a*b)/lcm(a, b) << endl;
-	
-	return 0;
+/**
+ * Purpose: Calculates the least common multiple between two integers.
+ * 
+ * Input: 	two integers a,b; 1 <= a,b <= 2.10^9
+ * Output: 	LCM(a,b)
+ */
+
+#include <iostream>
+using namespace std;
+
+unsigned long gcd(unsigned long a, unsigned long b)
+{
+	if(b == 0) return a;
+	return gcd(b, a%b);
+}
+
+int main() 
+{
+	unsigned long a, b;
+	cin >> a >> b;
+
+	if(a < b) swap(a,b);
+
+	// a*b = LCM(a,b)*GCD(a,b)
+	cout << (a*b)/gcd(a,b) << endl;
+
+	return 0;
+
 }
