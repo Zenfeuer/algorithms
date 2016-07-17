ecb2eb3274e142895380f39af8acfe43bf3df50e
Darwing Hernandez <dhernandez@m5-software.com>
2016-07-16 23:09:21 -0400

    Fix paste error and document code

--- last_digit_large_fibonacci.cpp
+++ last_digit_large_fibonacci.cpp
@@ -1,28 +1,39 @@
-#include <iostream>
-using namespace std;
-
-int main() 
-{
-	int n;
-	unsigned long prevElem1 = 0;
-	unsigned long prevElem2 = 1;
-	unsigned long currentElem = 0;
-	
-	cin >> n;
-	
-	if(n == 0) cout << 0 << endl;
-	else if(n == 1) cout << 1 << endl;
-	else
-	{
-		while(n-- >= 2)
-		{
-			currentElem = (prevElem1%10 + prevElem2%10)%10;
-			prevElem1 = prevElem2;
-			prevElem2 = currentElem;
-		}
-		
-		cout << currentElem << endl;
-	}
-	
-	return 0;
+/**
+ * Purpose: Calculates Fib(n) mod 10 in iterative way
+ *
+ * Fib(0) = 0; Fib(1) = 1; Fib(n) mod 10 = [Fib(n-2) mod 10 + Fib(n-1) mod 10] mod 10
+ *
+ * Input: 	integer n; 0 <= n <= 10^7
+ * Output: 	Fib(n) mod 10
+ */
+
+#include <iostream>
+using namespace std;
+typedef unsigned long long ull;
+
+ull fibonacciMod10(ull n)
+{
+	ull prevElem1   = 0;	// Base case Fib(0)
+	ull prevElem2   = 1;	// Base case Fib(1)
+	ull currentElem = 0;
+
+	currentElem = n;
+
+	while(n-- >= 2)
+	{
+		currentElem = (prevElem1%10 + prevElem2%10)%10;
+		prevElem1 = prevElem2;
+		prevElem2 = currentElem;
+	}
+
+	return currentElem;
+}
+
+int main() 
+{
+	int n;
+	cin >> n;
+	cout << fibonacciMod10(n) << endl;
+
+	return 0;
 }
